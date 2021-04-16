8d27a9d3c62b0d0ee88c5a8a45b7c12fe374b5c8
Zabi <mzabiu@gmail.com>
2021-04-16 21:04:07 +0530

    Added new file

---
+++ src/test/java/com/blaze/pages/reserve/ReservePage.java
@@ -0,0 +1,28 @@
+package com.blaze.pages.reserve;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import com.blaze.pages.BasePage;
+
+public class ReservePage extends BasePage {
+
+	public String windowTitle = "BlazeDemo - reserve";
+
+	private String chooseThisFlight = "//td[text()='%flightName%']/preceding-sibling::td//input";
+
+	public ReservePage(EventFiringWebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void selectAirline(String airline, String time) {
+
+	}
+
+	public void selectAirlines(String airline) {
+		String xpath = chooseThisFlight.replace("%flightName%", airline);
+		getElement(By.xpath(xpath)).click();
+	}
+
+}
